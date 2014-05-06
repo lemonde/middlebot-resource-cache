dbb2ba7b218ebdfb59b6422ab42e4d3cf9599c36
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-06 12:01:02 +0200

    filled utils tests

--- test/utils.js
+++ test/utils.js
@@ -7,11 +7,17 @@ var cacheUtils = require('../lib/utils');
 
 describe('cache utils', function() {
   describe('#format', function() {
-
+    it('should format the cache key', function () {
+      expect(cacheUtils.format('test', 1, 'user'))
+      .to.equal('api:test:1:12dea96fec20593566ab75692c9949596833adc9');
+    });
   });
 
   describe('#formatMatcher', function() {
-
+    it('should format the glob pattern to match key', function () {
+      expect(cacheUtils.formatMatcher('test', 1))
+      .to.equal('api:test:1*');
+    });
   });
 });
 
