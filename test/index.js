894a8e4ca0c5121212d6eb88539b54bdb4bda642
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-06 11:56:12 +0200

    initial commit

---
+++ test/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var cacheMiddlewares = require('../lib');
+
+describe('cache middlewares', function() {
+  describe('#setCache', function() {
+
+  });
+
+  describe('#getCache', function() {
+
+  });
+
+  describe('#uncache', function() {
+
+  });
+});
+
+
---
+++ test/utils.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var cacheUtils = require('../lib/utils');
+
+describe('cache utils', function() {
+  describe('#format', function() {
+
+  });
+
+  describe('#formatMatcher', function() {
+
+  });
+});
+
+
